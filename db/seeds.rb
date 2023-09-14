dd6bfa5ad32d94207744084718d5c8299d9aae7d
Justin Ko <14007+justinko@users.noreply.github.com>
2023-09-14 11:08:50 -0600

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create!(email: "john.doe@test.com", password: "password1234")
+User.create(email: "john.doe@test.com", password: "password1234")
